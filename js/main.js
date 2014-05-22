e8c84e2c92e8d19523b98257fa105f521de49b3d
Khaled Helmy <khaled.a.helmy@gmail.com>
2014-05-22 22:37:58 +0200

    Added Github account

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 jQuery(document).ready(function($) {
 	$('#normal-button').toolbar({content: '#user-options', position: 'bottom'});
+	$('#normal-button').on('toolbarItemClick', function(event) {
+		window.location = arguments[1];
+	});
 });
